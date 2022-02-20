4bf0ad0a00d6180547ba4b0b41b5fa6a42370875
mikhail <m.m.dezhurko@gmail.com>
2022-02-20 03:50:57 +0300

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+/* eslint-disable import/no-extraneous-dependencies */
+const TerserPlugin = require('terser-webpack-plugin');
+/* eslint-enable import/no-extraneous-dependencies */
+
+const packageName = require('./package.json').name;
+
+module.exports = {
+  entry: './src/index.ts',
+  target: 'node', // in order to ignore built-in modules like path, fs, etc.
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devtool: false,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    library: {
+      name: packageName,
+      type: 'commonjs',
+    },
+    clean: true,
+  },
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          mangle: {
+            properties: {
+              // mangle properties with prefix _
+              regex: /^_[a-zA-Z]\w*/,
+            },
+          },
+        },
+      }),
+    ],
+  },
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+};
